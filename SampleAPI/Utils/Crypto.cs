239c258d17be0d1dd100d15e20c8f262eb74800b
MohammadAb1075 <mabdollah1375gmail.com>
2022-11-18 07:42:57 -0800

    AddCryptoUtil

---
+++ SampleAPI/Utils/Crypto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SampleAPI.Utils
+{
+    public class Crypto
+    {
+        public static string ToSHA512(string plain)
+        {
+            var SHA512 = System.Security.Cryptography.SHA512.Create();
+            var bytes = Encoding.UTF8.GetBytes(plain);
+            var hash = SHA512.ComputeHash(bytes);
+            //byte to hex string
+            return String.Concat(hash.Select(b => b.ToString("X2")).ToArray());
+        }
+
+    }
+}
