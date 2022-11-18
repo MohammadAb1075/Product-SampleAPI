4384d630d893ec0e60dd55200fd49505ff1a609a
MohammadAb1075 <mabdollah1375gmail.com>
2022-11-18 06:50:59 -0800

    AddUserModel

--- SampleAPI/Data/AppDbContext.cs
+++ SampleAPI/Data/AppDbContext.cs
@@ -1,26 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SampleAPI.Models;
 using SampleAPI.Models.Config;
+using Microsoft.Extensions.Configuration;
+using static SampleAPI.Program;
+using SampleAPI.Data.Config;
 
 namespace SampleAPI.Data
 {
     public class AppDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    var appSettings = Program.Configuration.GetSection("AppSettings").Get<AppSettings>();
+            //    optionsBuilder.UseSqlServer(appSettings.ConnectionString);
+            //}
+            //base.OnConfiguring(optionsBuilder);
 
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer("data source=.;initial catalog=DbSampleAPI;integrated security=True;multipleactiveresultsets=True;application name=DbSampleAPI");
             }
             base.OnConfiguring(optionsBuilder);
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new ProductConfig());
+            modelBuilder.ApplyConfiguration(new UserConfig());
 
             base.OnModelCreating(modelBuilder);
         }
---
+++ SampleAPI/Migrations/20221118144431_addUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleAPI.Migrations
+{
+    public partial class addUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("dee1f869-da00-468f-821a-617590d828d7"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValue: new Guid("672f71af-e8d2-4613-b72f-43af1f2793d6"));
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Salt = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("672f71af-e8d2-4613-b72f-43af1f2793d6"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldDefaultValue: new Guid("dee1f869-da00-468f-821a-617590d828d7"));
+        }
+    }
+}
