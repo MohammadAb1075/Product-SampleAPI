8b037436172ee8780a7a7750e05db5b7e8b06088
MohammadAb1075 <mabdollah1375gmail.com>
2022-11-19 01:16:36 -0800

    NSwag

--- SampleAPI/Startup.cs
+++ SampleAPI/Startup.cs
@@ -6,6 +6,18 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using SampleAPI.Data;
 using SampleAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.Swagger;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.SwaggerGen;
+using NSwag.Generation.Processors.Security;
+using NSwag;
+using OpenApiSecurityScheme = NSwag.OpenApiSecurityScheme;
+using System.Linq;
 
 namespace SampleAPI
 {
@@ -22,13 +34,40 @@ namespace SampleAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SampleAPI", Version = "v1" });
+            //    c.DescribeAllEnumsAsStrings();
+            //    c.DescribeAllParametersInCamelCase();
+            //});
+
+            //services.AddSwaggerGen();
+            //services.ConfigureSwaggerGen(options =>
+            //{
+            //    options.SwaggerDoc("v1", new OpenApiInfo { Title = "SampleAPI", Version = "v1" });
+            //    options.DescribeAllEnumsAsStrings();
+            //});
+
+
+            //services.AddSwaggerDocument();
+
+            services.AddSwaggerDocument(config =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SampleAPI", Version = "v1" });
-                c.DescribeAllEnumsAsStrings();
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "Product Sample API";
+                    document.Info.Description = "Web API";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Mohammad Abdollahzadeh",
+                        Email = "mabdollah1375@gmail.com"
+                    };
+                };
             });
 
-                services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>();
             services.AddScoped<IRepository, Repository>();
 
             //var builder = new ConfigurationBuilder()
@@ -43,8 +82,17 @@ namespace SampleAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleAPI v1"));
+
+
+                app.UseOpenApi();
+                app.UseSwaggerUi3();
+
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c =>
+                //{
+                //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleAPI V1");
+                //});
+
             }
 
             app.UseHttpsRedirection();
