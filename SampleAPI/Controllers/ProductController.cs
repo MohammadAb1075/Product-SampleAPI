9584c20574d46a8595e03e10a73ce10a4ab77b4e
MohammadAb1075 <mabdollah1375gmail.com>
2022-11-19 07:24:28 -0800

    SetProductCRUD

--- SampleAPI/Controllers/ProductController.cs
+++ SampleAPI/Controllers/ProductController.cs
@@ -4,11 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using SampleAPI.Data;
 using SampleAPI.Models;
 using SampleAPI.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Http.ModelBinding;
-
 namespace SampleAPI.Controllers
 {
 
@@ -25,18 +24,21 @@ namespace SampleAPI.Controllers
             db = new Repository(); 
         }
 
-        private async Task<List<Product>> ProductsFound(int page, int limit)
-        {
-            int skip = (page - 1) * limit;
-            var result = await db.GetAllAsync<Product>();
-            result = result.Skip(skip).Take(limit).OrderBy(x => x.Id).ToList();
-            return result;
-        }
+        //private async Task<List<Product>> ProductsFound(int page, int limit)
+        //{
+        //    int skip = (page - 1) * limit;
+        //    var result = await db.GetAllAsync<Product>();
+        //    result = result.Skip(skip).Take(limit).OrderBy(x => x.Id).ToList();           
+        //    return result;
+        //}
 
         [HttpGet]
         public async Task<IActionResult> Get(int page = 1, int limit = 5)
         {
-            return Ok(await ProductsFound(page, limit));
+            int skip = (page - 1) * limit;
+            var result = await db.GetAllAsync<Product>();
+            result = result.Skip(skip).Take(limit).OrderBy(x => x.Id).ToList();
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
@@ -56,10 +58,20 @@ namespace SampleAPI.Controllers
 
         
         [HttpGet("Search/")]
-        public async Task<IActionResult> Get(string type, string color,int page = 1, int limit = 5)
+        public async Task<IActionResult> Get(TypeEnum type, ColorEnum color,int page = 1, int limit = 5)
         {
-            var result = await ProductsFound(page, limit);
-            //result = result.Where(x => x.TypeEquals(type)).ToList();
+            int skip = (page - 1) * limit;
+            var result = await db.GetAllAsync<Product>();           
+            if ((int)type != 0)
+            {
+                result = result.Where(x => x.Type == type).ToList();
+            }
+            
+            if(color != 0)
+            {
+                result = result.Where(x => x.Color == color).ToList();
+            }
+            result = result.Skip(skip).Take(limit).OrderBy(x => x.Id).ToList();
             if (result == null)
             {
                 return BadRequest("There is'nt Any product with this information");
@@ -72,26 +84,40 @@ namespace SampleAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] Product model)
+        public async Task<IActionResult> Create([FromBody] ProductViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest("Information Is Not True");
             }
-            await db.CreateAsync<Product>(model);
+            await db.CreateAsync<Product>(new Product {Title = model.Title, Price = model.Price, Color = model.Color, Type = model.Type});
             return Ok();
         }
 
-        [HttpPut("{model}")]
-        public async Task<IActionResult> Edit([FromBody] Product model)
+        //[HttpPut()]
+        //public async Task<IActionResult> Edit([FromBody] Product model)
+        //{
+        //    //if (!ModelState.IsValid)
+        //    //{
+        //    //    return BadRequest("Information Is Not True");
+        //    //}
+        //    await db.UpdateAsync<Product>(model);
+        //    return Ok();
+        //}
+
+        [HttpPut()]
+        public async Task<IActionResult> Edit(string id, [FromBody] Product model)
         {
-            //if (!ModelState.IsValid)
-            //{
-            //    return BadRequest("Information Is Not True");
-            //}
+            var entity = await db.GetByIdAsync<Product>(id);
+            if (entity == null)
+            {
+                return NotFound("There Isn't Any Product Whit This Id");
+            }
             await db.UpdateAsync<Product>(model);
+            //await db.UpdateAsync<Product>(new Product {Id = id, Title = model.Title, Price = model.Price, Color = model.Color, Type = model.Type});
             return Ok();
         }
+
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(string id)
@@ -103,7 +129,5 @@ namespace SampleAPI.Controllers
             await db.DeleteAsync<Product>(id);
             return Ok();
         }
-
-
     }
 }
