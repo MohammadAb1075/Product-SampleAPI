cecfb739f0314c931892cd461962d2675a327bdf
MohammadAb1075 <mabdollah1375gmail.com>
2022-11-19 10:32:51 -0800

    AddViewModel

--- SampleAPI/Models/ProductViewModel.cs
+++ SampleAPI/Models/ProductViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using SampleAPI.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace SampleAPI.Models
 {
---
+++ SampleAPI/Models/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleAPI.Models
+{
+    public class RegisterViewModel
+    {
+        
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).{8,}$")]
+        //[StringLength(256, ErrorMessage = "Confirm password doesn't match, Try again !")]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(256, MinimumLength = 4, ErrorMessage = "{0} Length Must Be Between{1} & {2} Characters.")]
+        public string Password { get; set; }
+
+        //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).{8,}$")]
+        //[StringLength(256, ErrorMessage = "Confirm password doesn't match, Try again !")]
+        [Required(ErrorMessage = "Password is required")]
+        [Compare("Password", ErrorMessage = "{0} Length Must Be Between{1} & {2} Characters.")]
+        [StringLength(256, MinimumLength = 4, ErrorMessage = "Confirm password doesn't match, Try again !")]
+        public string ConfirmPassword { get; set; }
+    }
+}
