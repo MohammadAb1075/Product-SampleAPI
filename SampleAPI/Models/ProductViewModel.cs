221885a407eb4481be8cb25cda11ff0e7734d9b7
MohammadAb1075 <mabdollah1375gmail.com>
2022-11-19 07:23:09 -0800

    AddProductViewModel

---
+++ SampleAPI/Models/ProductViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleAPI.Models
+{
+    public class ProductViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public TypeEnum Type { get; set; }
+        
+        [Required]
+        public double Price { get; set; }
+        
+        [Required]
+        public ColorEnum Color { get; set; }
+
+    }
+}
